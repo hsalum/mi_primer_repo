a32e555ff4124bd1e32dd46f3c5d81a46ebe7683
Hassan Salum <hassalum@hotmail.com>
2025-06-17 19:58:17 -0300

    se agrego la funcion hola mundo al script

---
+++ ejepmlo_git.py
@@ -0,0 +1,6 @@
+def hola_mundo():
+    print("Hola, mundo!")       
+# Ejemplo de uso
+if __name__ == "__main__":
+    hola_mundo()
+    # Aquí puedes agregar más código para probar la función o realizar otras tareas.    
